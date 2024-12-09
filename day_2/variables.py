7d0b5107affed772fa4c4c5db43284838c93acc9
Rashford2034 <abdulrasheed2034@gmail.com>
2024-12-09 09:53:02 -0800

    Updated file

--- day_2/variables.py
+++ day_2/variables.py
@@ -20,7 +20,7 @@ city = 'Lagos'
 
 print(city)
 
-age = 30
+age = 32
 
 print(age)
 
@@ -43,5 +43,79 @@ print(is_light_on)
 First_name, last_name, country, age, is_married = 'Abdulrasheed', 'Mohammed', 'Nigeria', 32, False
 
 print(First_name, last_name, country, age, is_married)
+
+print(type('Abdulrasheed'))  
+
+print(type(32))
+
+print(type(False))
+
+print(type(True))
+
+print(type('Mohammed'))
+
+print(type(2024))
+
+print(type('Nigeria'))
+
+# exercise level 2
+
+print(len('Abdulrasheed'))
+
+print(len('Mohammed'))
+
+num_one = 5
+
+num_two = 4
+
+print(num_one + num_two)
+
+total = 9
+
+print(num_one - num_two)
+
+diff = 1
+
+print(num_two * num_one )
+
+product = 20
+
+print(num_one / num_two)
+
+division = 1.25
+
+print(num_two % num_one)
+
+remainder = 4
+
+print(num_one ** num_two)
+
+exp = 625
+
+print(num_one // num_two)
+
+floor_division = 1
+
+# Python program to find Area of a circle
+
+import math
+
+area_of_circle = math.pi * 30 ** 2
+
+print(area_of_circle)
+
+circum_of_circle = math.pi * 2 * 30
+
+print(circum_of_circle)
+
+area_of_circle = 2827
+
+circum_of_circle = 188
+
+
+
+
+
+
 
 
