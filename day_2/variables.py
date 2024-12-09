730b72da4314e537d1d56b04475f27de39baf4d0
Rashford2034 <abdulrasheed2034@gmail.com>
2024-12-09 07:42:49 -0800

    UPDATED FILE

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,5 +1,47 @@
+# 'Day 2: 30 Days of python programming'
+
 first_name = 'Abdulrasheed'
 
 print(first_name)
+
+last_name = 'Mohammed'
+
+print(last_name)
+
+full_name = 'Abdulrasheed Mohammed'
+
+print(full_name)
+
+country = 'Nigeria'
+
+print(country)
+
+city = 'Lagos'
+
+print(city)
+
+age = 30
+
+print(age)
+
+year = 2024
+
+print(year)
+
+is_married = False
+
+print(is_married)
+
+is_true = False
+
+print(is_true)
+
+is_light_on = False
+
+print(is_light_on)
+
+First_name, last_name, country, age, is_married = 'Abdulrasheed', 'Mohammed', 'Nigeria', 32, False
+
+print(First_name, last_name, country, age, is_married)
 
 
